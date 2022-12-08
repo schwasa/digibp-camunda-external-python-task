72ec4cb9617d34de9bd6f924ce6048d135ba4f3a
schwasa <102740850+schwasa@users.noreply.github.com>
2022-12-08 17:05:33 +0100

    Add files via upload

---
+++ CheckDocument.py
@@ -0,0 +1,26 @@
+import httpimport
+with httpimport.github_repo('DigiBP', 'digibp-camunda-external-python-task', 'cam'): import cam
+
+class CheckDocument:
+    def __init__(self):
+        self.worker = cam.Client("https://digibp.herokuapp.com/engine-rest")
+        self.worker.subscribe("CheckDocument", self.get_surprise_menu_callback, "saechsiluute")
+        self.worker.polling()
+
+    def check_document(self, taskid, response):
+        try:
+            vegetarian_guests = response[0]['variables']['input']['value']
+        except:
+            vegetarian_guests = False
+
+        if vegetarian_guests:
+            score = "True"
+        else:
+            score = "False"
+
+        variables = {"score": score}
+        self.worker.complete(taskid, **variables)
+
+
+if __name__ == '__main__':
+    CheckDocument()
