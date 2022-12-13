0774bb03842955d13896c89a7c8c1b6a844180fd
schwasa <102740850+schwasa@users.noreply.github.com>
2022-12-13 19:49:44 +0100

    Update surprise_menu_client.py

--- surprise_menu_client.py
+++ surprise_menu_client.py
@@ -1,7 +1,8 @@
 import cam
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from text import TfidfVectorizer
+from pairwise import cosine_similarity
 import docx2txt
+import gdown
 
 class SurpriseMenuClient:
     def __init__(self):
@@ -10,29 +11,54 @@ class SurpriseMenuClient:
         self.worker.polling()
 
     def get_surprise_menu_callback(self, taskid, response):
+        '''First get the variables from the process'''
         try:
-            duc_url = response[0]['variables']['doc_url']['value']
+            doc_url = response[0]['variables']['doc_url']['value']
         except:
             doc_url = False
 
         try:
-            swissmedic_id = response[1]['variables']['swissmedic_id']['value']
+            swissmedic_id = response[0]['variables']['swissmedic_id']['value']
         except:
             swissmedic_id = False
-        
-        if doc_url:
-            score = 0.6
-         else:
-            score = 0.01
-        
-	input_file = docx2txt.process("Benchmark.docx")
 
-        if swissmedic_id in input_file:
-		    swissmedic_id = swissmedic_id
-	    else:
-		    swissmedic_id = "Error: Swissmedic ID is not in accordance with Approved Drug Information"
+        try:
+            case_id = response[0]['variables']['case_id']['value']
+        except:
+            case_id = False
 
-        variables = {"score": score, "swissmedic_id": swissmedic_id}
+        '''Download the document to local storage to access properly'''
+        output = "{0}_InputDoc.docx".format(case_id)
+        gdown.download(doc_url, output, quiet=False, fuzzy=True)
+
+        '''Document comparing based on Tfidf'''
+
+        base_document = docx2txt.process(output)
+        documents = [docx2txt.process("Benchmark.docx")]
+
+        vectorizer = TfidfVectorizer()
+
+        # To make uniformed vectors, both documents need to be combined first.
+        documents.insert(0, base_document)
+        embeddings = vectorizer.fit_transform(documents)
+
+        cosine_similarities = cosine_similarity(embeddings[0:1], embeddings[1:]).flatten()
+
+        highest_score = 0
+        highest_score_index = 0
+        for i, score in enumerate(cosine_similarities):
+            if highest_score < score:
+                highest_score = score
+                highest_score_index = i
+
+        comp_score = highest_score
+        doc_check_message = 'Document was successfully checked'
+
+        if doc_url == False:
+            comp_score = 0
+            doc_check_message = 'doc_url is missing'
+
+        variables = {"comp_score": comp_score, "swissmedic_id": swissmedic_id, "doc_check_message": doc_check_message}
         self.worker.complete(taskid, **variables)
 
 
